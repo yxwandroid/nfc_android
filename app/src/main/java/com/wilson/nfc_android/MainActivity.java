547d8ba50100f280b80a493b137b7a07afbea468
xuewu.yang <xuewu.yang@leediancn.com>
2018-07-05 16:12:55 +0800

    init

--- app/src/main/java/com/wilson/nfc_android/MainActivity.java
+++ app/src/main/java/com/wilson/nfc_android/MainActivity.java
@@ -47,6 +47,7 @@ public class MainActivity extends BaseActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        //设置默认选中
         getSupportFragmentManager().beginTransaction().replace(R.id.flContent, new FragmentOne()).commit();
         setTitle("我的动态");
     }
@@ -86,23 +87,6 @@ public class MainActivity extends BaseActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-
-//        int id = item.getItemId();
-//
-//        if (id == R.id.nav_camera) {
-//            // Handle the camera action
-//        } else if (id == R.id.nav_gallery) {
-//
-//        } else if (id == R.id.nav_slideshow) {
-//
-//        } else if (id == R.id.nav_manage) {
-//
-//        } else if (id == R.id.nav_share) {
-//
-//        } else if (id == R.id.nav_send) {
-//
-//        }
 
         Fragment fragment = null;
         Class fragmentClass;
